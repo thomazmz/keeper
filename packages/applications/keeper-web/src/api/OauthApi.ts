3a6b54f1b651183a88e21c01252c47a398d91234
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2026-02-01 22:02:36 -0800

    Small schema updates on @keeper/ui

--- packages/applications/keeper-web/src/api/OauthApi.ts
+++ packages/applications/keeper-web/src/api/OauthApi.ts
@@ -3,7 +3,6 @@ import z from 'zod';
 import { HttpClient } from '../http/HttpClient';
 
 export const ConnectGoogleResponseSchema = z.object({
-  accessToken: z.string(),
   refreshToken: z.string(),
 })
 
--- packages/applications/keeper-web/src/hooks/useGoogleOauth.ts
+++ packages/applications/keeper-web/src/hooks/useGoogleOauth.ts
@@ -1,4 +1,3 @@
-// import { useMutation } from '@tanstack/react-query';
 import * as React from 'react';
 
 import { OauthApi } from '../api/OauthApi';
