07b325a63b6acb2e40f56bae83ab83af4a09b4bc
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2026-02-01 22:20:05 -0800

    Simplify @keeper/ui response types

--- packages/applications/keeper-web/src/api/OauthApi.ts
+++ packages/applications/keeper-web/src/api/OauthApi.ts
@@ -3,7 +3,14 @@ import z from 'zod';
 import { HttpClient } from '../http/HttpClient';
 
 export const ConnectGoogleResponseSchema = z.object({
+  id: z.string(),
+  createdAt: z.string(),
+  updatedAd: z.string(),
+  email: z.string(),
+  sourceId: z.string(),
   refreshToken: z.string(),
+  refreshTokenExpiresAt: z.number(),
+  refreshTokenExchangedAt: z.number(),
 })
 
 export declare namespace OauthApi {
