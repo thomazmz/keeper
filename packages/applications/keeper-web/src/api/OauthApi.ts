01ea463c0d8844e1642a0425543d07b83852b675
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2026-02-03 17:31:00 -0800

    Fix expected oauth zod schema

--- packages/applications/keeper-web/src/api/OauthApi.ts
+++ packages/applications/keeper-web/src/api/OauthApi.ts
@@ -3,14 +3,7 @@ import z from 'zod';
 import { HttpClient } from '../http/HttpClient';
 
 export const ConnectGoogleResponseSchema = z.object({
-  id: z.string(),
-  createdAt: z.string(),
-  updatedAd: z.string(),
-  email: z.string(),
-  sourceId: z.string(),
-  refreshToken: z.string(),
-  refreshTokenExpiresAt: z.number(),
-  refreshTokenExchangedAt: z.number(),
+  email: z.string()
 })
 
 export declare namespace OauthApi {
