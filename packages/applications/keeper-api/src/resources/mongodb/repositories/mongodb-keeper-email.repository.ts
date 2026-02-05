d3eb4cad7adcb7002b0b5379d51fa4d523fc7d68
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2026-02-04 18:02:03 -0800

    Implement MongodbKeeperEmalRepoitory

---
+++ packages/applications/keeper-api/src/resources/mongodb/repositories/mongodb-keeper-email.repository.ts
@@ -0,0 +1,31 @@
+import { MongoClient } from 'mongodb'
+import { KeeperEmail } from 'domain/domain.library'
+import { MongodbDocument } from "@nodelith/mongodb"
+import { MongodbRepository } from "@nodelith/mongodb"
+import { KeeperEmailRepository } from "domain/domain.library"
+
+export class MongodbKeeperEmailRepository extends MongodbRepository<KeeperEmail> implements KeeperEmailRepository {
+  private static readonly COLLECTION_NAME = 'keeper_email'
+  private static readonly DATABASE_NAME = 'keeper_database'
+
+  public constructor(mongodbClient: MongoClient) {
+    super(MongodbKeeperEmailRepository.COLLECTION_NAME, MongodbKeeperEmailRepository.DATABASE_NAME, mongodbClient)
+    this.collection.createIndex({ source: 1, sourceId: 1 }, { unique: true })
+  }
+
+  protected map(document: MongodbDocument<KeeperEmail>): KeeperEmail {
+    return {
+      id: document._id.toHexString(), 
+      createdAt: document.createdAt,
+      updatedAt: document.updatedAt,
+      date: document.date,
+      sender: document.sender,
+      subject: document.subject,
+      source: document.source,
+      sourceId: document.sourceId,
+      recipient: document.recipient,
+      // data: document.data,
+      // type: document.type,
+    }
+  }
+}
